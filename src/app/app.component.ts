f33b2f57b0469cfefe6f2ae6299040d75e4548de
funny860 <phanindrarock@gmail.com>
2021-12-22 13:02:15 +0530

    commit to master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+      .greater {
+        color: white;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'third-assignment';
+  dd = false;
+  buttonLog: number[] = [];
+
+  displayDetails() {
+    this.dd = this.dd == true ? false : true;
+    this.buttonLog.push(Date.now());
+  }
 }
